3d588ff317a6859501100214f29b4e71dbcc96e5
KennyCrump <kennycrump@gmail.com>
2019-02-21 18:58:21 -0700

    another attempt to fix hosted contact issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -120,13 +120,20 @@ class HomeIndex extends React.Component {
                         <div className="row">
                             <div className="8u 12u$(small)">
                                 <form method="post" name='contact' data-netlify='true' netlify data-netlify-honeypot='bot-field' action="#">
+                                <input type="hidden" name="form-name" value="contact" />
+                                <p hidden>
+                                    <label>
+                                    Don’t fill this out:{" "}
+                                    <input name="bot-field" onChange={this.handleChange} />
+                                    </label>
+                                </p>
+                                
+
                                     <div className="row uniform 50%">
                                         <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
                                         <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
                                         <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
-                                        <input type="hidden" name="form-name" value="contact" />
-                                        <p hidden> <label htmlFor="bot-field">Don’t fill this out:{' '}<input name="bot-field" /> </label> </p>
-                                    </div>
+                                         </div>
                                 </form>
                                 <ul className="actions">
                                     <li><input type="submit" value="Send Message" /></li>
